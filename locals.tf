c392c8d9b39902099aa808ce5197adf9b1d8c73f
fortidg <dgooch@fortinet.com>
2023-07-13 12:03:34 +0000

    updated local

--- locals.tf
+++ locals.tf
@@ -5,7 +5,7 @@ locals {
       disk = "fgt-log-disk-student1"
       p1ip = "192.168.128.11"
       p2ip = "192.168.129.11"
-      p3ip = "192.169.130.11"
+      p3ip = "192.168.130.11"
       license_file = "student1.lic"
     }
           
