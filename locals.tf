1392267ab71863dfefc9c8b24cc99577e964d65d
fortidg <dgooch@fortinet.com>
2023-07-31 18:40:11 +0000

    updating locals with correct networks

--- locals.tf
+++ locals.tf
@@ -2,23 +2,23 @@ locals {
   students = {
     "student1" = {
       name    = "student1"
-      trust_range = "192.168.1.0/24"
+      trust_range = "10.10.1.0/24"
       tools_range = "10.0.1.0/24"
-      p2ip = "192.168.1.10"
+      p2ip = "10.10.1.10"
       p3ip = "10.0.1.10"
-      kalip = "192.168.1.11"
-      decp1 = "10.0.1.13"
-      decp2 = "192.168.1.13"
+      kalip = "10.10.1.201"
+      decp1 = "10.0.1.100"
+      decp2 = "10.10.1.11"
     }
     "student2" = {
       name    = "student2"
-      trust_range = "192.168.2.0/24"
+      trust_range = "10.10.2.0/24"
       tools_range = "10.0.2.0/24"
-      p2ip = "192.168.2.10"
+      p2ip = "10.10.2.10"
       p3ip = "10.0.2.10"
-      kalip = "192.168.2.11"
-      decp1 = "10.0.2.13"
-      decp2 = "192.168.2.13"
+      kalip = "10.10.2.201"
+      decp1 = "10.0.2.100"
+      decp2 = "10.10.2.11"
     }
           
   }
